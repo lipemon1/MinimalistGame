8a843e4db1516a8f50fe9affa22087c9e7c5ccb2
Ramon Ferreira <ramon_felipe@outlook.com>
2021-09-18 11:25:01 -0300

    Adding our first connection prefab to the game

---
+++ Assets/Scripts/Gameplay/ConnectionBehavior.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MinimalGame.Gameplay.Connections
+{
+    public class ConnectionBehavior : MonoBehaviour
+    {
+        [SerializeField] float rotateAmount;
+        [SerializeField] float rotationSpeed = 0.1f;
+        
+        [Space]
+        [SerializeField] Vector3 debugFrom;
+        [SerializeField] Vector3 debugTo;
+        [SerializeField] float debugDif;
+        
+        [SerializeField] bool isRotating;
+        Quaternion from;
+        Quaternion to;
+        void Update()
+        {
+            if (isRotating)
+            {
+                Rotating();
+                TryStopRotation();
+            }
+        }
+
+        void OnMouseDown()
+        {
+            CalculateNewRotation(this.transform.rotation, rotateAmount, ref from, ref to);
+        }
+
+        void CalculateNewRotation(Quaternion curRotation, float rotateAmount, ref Quaternion from, ref Quaternion to)
+        {
+            isRotating = false;
+            Vector3 curRotVector3 = curRotation.eulerAngles;
+            Vector3 targetRotation = new Vector3(curRotVector3.x, (curRotVector3.y + rotateAmount), curRotation.z);
+            from = curRotation;
+            to = Quaternion.Euler(targetRotation);
+
+            debugFrom = from.eulerAngles;
+            debugTo = to.eulerAngles;
+            isRotating = true;
+        }
+
+        void TryStopRotation()
+        {
+            //TODO fix this bug - Lerp is not running smooth
+            debugDif = Math.Abs(from.eulerAngles.y) - Math.Abs(to.eulerAngles.y);
+            
+            // if (debugDif < 0.001f)
+            //     isRotating = false;
+        }
+
+        void Rotating()
+        {
+            transform.rotation = Quaternion.Lerp(from, to, Time.time * rotationSpeed);
+        }
+    }   
+}
