d6e6f88e285ea37fc9aa64f4cb6ad53aa0cd4eaf
Ramon Ferreira <ramon_felipe@outlook.com>
2021-09-19 19:35:04 -0300

    Creating the first 3 levels

--- Assets/Scripts/Gameplay/Connection/Components/Rotation/RotationBehavior.cs
+++ Assets/Scripts/Gameplay/Connection/Components/Rotation/RotationBehavior.cs
@@ -5,17 +5,8 @@ using UnityEngine;
 
 namespace MinimalGame.Gameplay.Connections
 {
-    public enum ConnectionPositionMode
-    {
-        Vertical = 0,
-        RightCorner = 1,
-        Horizontal = 2,
-        LeftCorner = 3
-    }
-    
     public class RotationBehavior : ConnectionBaseComponent
     {
-        [SerializeField] public ConnectionPositionMode CurPositionMode;
         [SerializeField] float rotateAmount;
 
         Quaternion from;
@@ -37,24 +28,11 @@ namespace MinimalGame.Gameplay.Connections
                 canInteractWith = false;
             
             CalculateNewRotation(this.transform.rotation, rotateAmount, ref from, ref to);
-            CalculationNewPositionMode();
 
             yield return new WaitForSeconds(0.1f);
             ConnectionBehavior.MarkConnectionAsRotated();
 
             canInteractWith = true;
-        }
-
-        void CalculationNewPositionMode()
-        {
-            int curPos = (int) CurPositionMode;
-            
-            if (curPos == 3)
-                curPos = 0;
-            else
-                curPos += 1;
-
-            CurPositionMode = (ConnectionPositionMode) curPos;
         }
 
         void CalculateNewRotation(Quaternion curRotation, float rotateAmount, ref Quaternion from, ref Quaternion to)
